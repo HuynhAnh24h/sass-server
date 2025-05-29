0bb536d8e84d2991129648f6ed1438c4c2e7ef01
HuynhAnh24h <anhdev24h@gmail.com>
2025-05-29 08:01:23 +0700

    Test Deploy

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,7 +1,7 @@
 module.exports = {
     createUser: async (req,res) =>{
         try{
-            res.send("Huỳnh Anh Developer")
+            res.send("Deploy server Thành công")
         }catch(err){
             return res.status(500).json({
                 success: false,
